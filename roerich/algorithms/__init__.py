c2149b021e5655264fb7c581e6fb7f51d3146273
a007mg <alekseev.artem700@gmail.com>
2023-04-19 22:58:04 +0300

    ABS added

--- roerich/algorithms/__init__.py
+++ roerich/algorithms/__init__.py
@@ -1,8 +1,6 @@
 from .algorithms import OnlineNNClassifier, OnlineNNRuLSIF
 from .cpdc import ChangePointDetectionClassifier, ChangePointDetectionRuLSIF
 from .cpdc_abs import ChangePointDetectionClassifierABS
-from .cpdc_ebs import ChangePointDetectionClassifierEBS
-from .cpdc_cv import ChangePointDetectionClassifierCV
 from .models import GBDTRuLSIFRegressor
 from .models import RegressionNetwork, NNRuLSIFRegressor
 from .models import ClassificationNetwork, NNClassifier
@@ -13,8 +11,6 @@ __all__ = [
     'OnlineNNRuLSIF', 
     'ChangePointDetectionClassifier', 
     'ChangePointDetectionClassifierABS',
-    'ChangePointDetectionClassifierEBS',
-    'ChangePointDetectionClassifierCV', 
     'ChangePointDetectionRuLSIF'
     'ClassificationNetwork', 
     'NNClassifier'
