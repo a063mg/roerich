f5920e14ff129c9e0306d41116a5933e8cd914e5
a007mg <alekseev.artem700@gmail.com>
2023-04-19 23:00:01 +0300

    EBS added

--- roerich/algorithms/__init__.py
+++ roerich/algorithms/__init__.py
@@ -1,8 +1,6 @@
 from .algorithms import OnlineNNClassifier, OnlineNNRuLSIF
 from .cpdc import ChangePointDetectionClassifier, ChangePointDetectionRuLSIF
-from .cpdc_abs import ChangePointDetectionClassifierABS
 from .cpdc_ebs import ChangePointDetectionClassifierEBS
-from .cpdc_cv import ChangePointDetectionClassifierCV
 from .models import GBDTRuLSIFRegressor
 from .models import RegressionNetwork, NNRuLSIFRegressor
 from .models import ClassificationNetwork, NNClassifier
@@ -12,9 +10,7 @@ __all__ = [
     'OnlineNNClassifier', 
     'OnlineNNRuLSIF', 
     'ChangePointDetectionClassifier', 
-    'ChangePointDetectionClassifierABS',
     'ChangePointDetectionClassifierEBS',
-    'ChangePointDetectionClassifierCV', 
     'ChangePointDetectionRuLSIF'
     'ClassificationNetwork', 
     'NNClassifier'
